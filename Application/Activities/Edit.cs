8ffe6e9a5ccee11b98f56d06c1d9a3ef99fa01d9
Albert Valdes <albert.anthony1999@gmail.com>
2022-05-19 02:01:59 -0500

    feat: added better API responses for delete/eit handlers"

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Core;
 using AutoMapper;
 using Domain;
 using FluentValidation;
@@ -12,7 +13,7 @@ namespace Application.Activities
 {
     public class Edit
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Activity Activity { get; set; }
         }
@@ -25,7 +26,7 @@ namespace Application.Activities
             }
         }
 
-        public class Handler : IRequestHandler<Command>
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
@@ -35,16 +36,20 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Activity.Id);
                 
+                if (activity == null) return null;
+
                 // This method from AutoMapper will update each property of an activity object for us
                 _mapper.Map(request.Activity, activity);
 
-                await _context.SaveChangesAsync();
+                var result = await _context.SaveChangesAsync() > 0;
 
-                return Unit.Value;
+                if (!result) return Result<Unit>.Failure("Failed to Update activity");
+
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
